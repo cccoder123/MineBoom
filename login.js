0383a4dc5b675ebb1edce6b54d86adb3ad3d4d4b
cccoder123 <51296698+cccoder123@users.noreply.github.com>
2021-07-04 17:46:59 +0100

    Create login.js

---
+++ login.js
@@ -0,0 +1,17 @@
+function check(form) {
+    username = new Array("ccn5ta8pp", "jh7llpsws", "oyouiq0ig");
+    password = new Array("ccn5ta8pp", "jh7llpsws", "oyouiq0ig");
+
+	if(form.userid.value == username[0] && form.pswrd.value == password[0]||form.userid.value == username[1] && form.pswrd.value == password[1]) {
+		location.replace('beta')/* The Administrator homepage */
+	}
+	else if(form.userid.value == username[2] && form.pswrd.value == password[2]) {
+		location.replace('beta')/* The members homepage */
+	}oyouiq0ig
+	else if(form.userid.value == username[0]||form.userid.value == username[1]||form.userid.value == username[2]) {
+		alert("Incorrect key Confirmation")
+	}
+	else {
+		alert("Invalid key! Try again")
+	}
+}
